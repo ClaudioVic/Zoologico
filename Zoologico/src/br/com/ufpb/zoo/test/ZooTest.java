13e5499d0fd92386409291e1d63691025cadf082
robsonalvz <robson.alves@dce.ufpb.br>
2017-09-22 00:47:34 -0300

    test

--- Zoologico/src/br/com/ufpb/zoo/test/ZooTest.java
+++ Zoologico/src/br/com/ufpb/zoo/test/ZooTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import br.com.ufpb.zoo.control.SistemaZoo;
 import br.com.ufpb.zoo.model.Animal;
+import br.com.ufpb.zoo.model.Endereco;
+import br.com.ufpb.zoo.model.Funcionario;
 
 public class ZooTest {
 
@@ -11,6 +13,8 @@ public class ZooTest {
 	public void test() {
 		SistemaZoo sistema = new SistemaZoo();
 		Animal animal = new Animal("cachorro","Canis lupus","18/03/2010","teste");
+		Endereco end = new Endereco("rua aride", "55900", "BA", "rT", "12", "casa");
+		Funcionario f = new Funcionario("joao", "123123", "123123", "99545622",end , "alimentador", "....");
 		sistema.adicionaAnimal(animal);
 		
 	}
